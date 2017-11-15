152b6993725ae9ab701a0e53dd8c9f05cb48aefa
Takayuki Kaburagi <tkaburagi@pivotal.io>
2017-11-15 19:18:46 +0900

    Delete init-env.sh

--- init-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cf login -a api.system.kabdev.xyz --skip-ssl-validation -o pcfdemoapp -s staging -u kabu -p kabu
-cf delete pcfdemoapp -f 
-cf delete-route apps.kabdev.xyz --hostname pcfdemoapp-staging -f
-
-cf login -a api.system.kabdev.xyz --skip-ssl-validation -o pcfdemoapp -s production -u kabu -p kabu
-cf delete pcfdemoapp -f 
-cf delete-route apps.kabdev.xyz --hostname pcfdemoapp -f
